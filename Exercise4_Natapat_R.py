a9c7e155de351a83b8ca52cefeaa4e251caf0778
NTPNTPNTP <75570205+NTPNTPNTP@users.noreply.github.com>
2020-12-08 18:48:42 +0700

    Create Exercise4_Natapat_R.py

---
+++ Exercise4_Natapat_R.py
@@ -0,0 +1,16 @@
+"""
+FE = Foundation English
+GB = General Business
+ICS = Introduction to Computer Systems
+CP = Computer Programming
+"""
+FE = 80
+GB = 90
+ICS = 75
+CP = 85
+
+print('--- Your Score ---')
+print('Foundation English :', FE)
+print('General Business :', GB)
+print('Introduction to Computer Systems :', ICS)
+print('Computer Programming :', CP)
