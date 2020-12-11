943b9865e8f058729f4870b90ba1859b5957272d
NTPNTPNTP <75570205+NTPNTPNTP@users.noreply.github.com>
2020-12-11 14:13:34 +0700

    Create Lecture50_Natapat_R.py

---
+++ Lecture50_Natapat_R.py
@@ -0,0 +1,16 @@
+def addNumber(num_1, num_2):
+    print(num_1+num_2)
+
+def deleteNumber(num_1, num_2):
+    print(num_1-num_2)
+
+def multiplyNumber(num_1, num_2):
+    print(num_1*num_2)
+
+def divideNumber(num_1, num_2):
+    print(num_1/num_2)
+
+addNumber(10, 2)
+deleteNumber(10, 2)
+multiplyNumber(10, 2)
+divideNumber(10, 2)
